81eaad07df59b88b584a8604550124110c04141f
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2021-08-10 21:28:34 +0900

    fie

--- answer.py
+++ answer.py
@@ -4,8 +4,10 @@ list_a = [[[2, 3, 4, 2], [1, 3, 3]]]
 #     for ye in ii:
 #         print(ye)
 
-list_b = [[2, 3], [4, 5], [3, 4], [4, 5]]
-import numpy as np
-num_a = np.array(list_b, dtype=np.float32)
-print(num_a)
-print(type(num_a))
+# list_b = [[2, 3], [4, 5], [3, 4], [4, 5]]
+# import numpy as np
+# num_a = np.array(list_b, dtype=np.float32)
+# print(num_a)
+# print(type(num_a))
+list_a = [2, 3, 4, 3, 1, 4]
+print(list_a[:-1])
