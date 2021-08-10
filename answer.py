e88b4f59053f729ba36bb53048b6fea07896a8f5
tsuchidashinya <tsuchida.shinya413@mail.kyutech.jp>
2021-08-10 21:16:21 +0900

    fie

---
+++ answer.py
@@ -0,0 +1,11 @@
+list_a = [[[2, 3, 4, 2], [1, 3, 3]]]
+# print(len(list_a))
+# for ii in list_a:
+#     for ye in ii:
+#         print(ye)
+
+list_b = [[2, 3], [4, 5], [3, 4], [4, 5]]
+import numpy as np
+num_a = np.array(list_b, dtype=np.float32)
+print(num_a)
+print(type(num_a))
