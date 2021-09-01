10b34531b7e9d1fc44e2308732104734f3cd287e
tent-house <63469487+tent-house@users.noreply.github.com>
2021-09-01 14:31:54 +0900

    dsds

--- week_2/convolution.py
+++ week_2/convolution.py
@@ -21,21 +21,28 @@ t = [[[[0.6, 0.9], [0.5, 0.7]]]]  #教師行列
 x = torch.tensor(x, dtype=torch.float32)
 t = torch.tensor(t, dtype=torch.float32)
 print("入力は" + str(x))
+print("")
 print("教師データは" + str(t))
+print("")
 model = Model()
 y_pred = model(x)
 print("更新前の出力: " + str(y_pred))
+print("")
 print("更新前の重みとバイアスは" + str(model.state_dict()))
+print("")
 criterion = nn.MSELoss()    #損失関数の定義(平均二乗誤差)
 optimazar = optim.SGD(model.parameters(), lr=0.01)   #確率的勾配降下法でパラメータを更新する
 loss = criterion(y_pred, t)    
 print("backward")
 loss.backward()     #誤差逆伝搬を行う
 print("損失関数は" + str(loss))
+print("")
 print("1重みの勾配は" + str(model.conv1.weight.grad))
+print("")
 optimazar.step()   #重みの更新
 y_pred = model(x)
 print("更新後の出力: " + str(y_pred))
+print("")
 
 print("更新後の重みとバイアスは" + str(model.state_dict()))
 
